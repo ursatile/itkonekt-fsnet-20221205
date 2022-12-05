1bc2f32ecd8d087a3eef2123da86477e56af9480
Dylan Beattie <dylan@dylanbeattie.net>
2022-12-05 11:46:36 +0000

    Artists controller with Index method returning ViewREsult

---
+++ Tikitapp/Tikitapp.Website/Controllers/ArtistsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Tikitapp.Website.Data;
+
+public class ArtistsController : Controller {
+	private readonly ILogger<ArtistsController> logger;
+	private readonly TikitappDbContext db;
+
+	public ArtistsController(ILogger<ArtistsController> logger, TikitappDbContext db) {
+		this.logger = logger;
+		this.db = db;
+	}
+    
+    public IActionResult Index() {
+        var artists = db.Artists.ToList();
+        return View(artists);        
+    }
+}
