bdce2cfcf717a66c0aaf3f8c7ae44b7f7a410524
Dylan Beattie <dylan@dylanbeattie.net>
2022-12-05 09:21:27 +0000

    dotnet new mvc and dotnet new xunit

---
+++ Tikitapp/Tikitapp.Website.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Tikitapp.Website.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tikitapp/Tikitapp.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tikitapp.Website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
